2b1c9a91aa5d9019927c6c9eb023d8bdd1425fe4
alan-finn <acfinn@gmail.com>
2017-02-14 12:51:13 -0600

    .

--- redirect.js
+++ redirect.js
@@ -1,8 +1,7 @@
 (function() {
 	var urlpathname = location.pathname;
 	var test = urlpathname.includes("2016/04/06/citrix-user-profile-store-cleaner");
-	//if (test) {
-	window.alert("sometext");	
+	if (test) {	
 	window.location.replace("http://www.afinn.net/citrix/citrix-user-store-profile-cleaner");
-	//}
+	}
 })();
